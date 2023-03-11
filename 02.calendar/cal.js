4776650c8efb3f2d9c78850f0083da6d9676c760
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-03-11 18:39:19 +0900

    padStartの不要な引数の削除 + 変数名の変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -5,22 +5,19 @@ const year = args.y || today.getFullYear();
 const correctionValue = 1;
 const startDate = new Date(year, month - correctionValue);
 const endDate = new Date(year, month, 0);
-const defaultSpaceValue = "   ";
+const defaultSpace = "   ";
 const rightJustifiedValue = 2;
 
 console.log(`     ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
-const defaultSpace = defaultSpaceValue.repeat(startDate.getDay());
-process.stdout.write(defaultSpace);
+const firstWeeksSpace = defaultSpace.repeat(startDate.getDay());
+process.stdout.write(firstWeeksSpace);
 for (
   startDate;
   startDate <= endDate;
   startDate.setDate(startDate.getDate() + 1)
 ) {
-  let day = String(startDate.getDate()).padStart(
-    rightJustifiedValue,
-    defaultSpaceValue
-  );
+  let day = String(startDate.getDate()).padStart(rightJustifiedValue);
   if (startDate.getDay() == 6) {
     console.log(day);
   } else {
