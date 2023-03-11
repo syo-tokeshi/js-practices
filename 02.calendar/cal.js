6e75f86f66db336c3cd9c0354b5c32ddfe5c43ac
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-03-11 21:56:27 +0900

    大文字にすべき定数を大文字に変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,16 +1,16 @@
 const args = require("minimist")(process.argv.slice(2));
 
 const today = new Date();
-const monthCorrectlyGetForValue = 1;
-const month = args.m || today.getMonth() + monthCorrectlyGetForValue;
+const VALUE_FOR_GET_MONTH_CORRECTLY = 1;
+const month = args.m || today.getMonth() + VALUE_FOR_GET_MONTH_CORRECTLY;
 const year = args.y || today.getFullYear();
-const startDate = new Date(year, month - monthCorrectlyGetForValue);
-const getMonthEndForValue = 0;
-const endDate = new Date(year, month, getMonthEndForValue);
+const startDate = new Date(year, month - VALUE_FOR_GET_MONTH_CORRECTLY);
+const VALUE_FOR_GET_MONTH_END = 0;
+const endDate = new Date(year, month, VALUE_FOR_GET_MONTH_END);
 
 const defaultSpace = "   ";
-const rightJustifiedValue = 2;
-const saturday = 6;
+const RIGHT_JUSTIFIED_LENGTH = 2;
+const SATURDAY = 6;
 
 console.log(`     ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
@@ -21,8 +21,8 @@ for (
   date <= endDate;
   date.setDate(date.getDate() + 1)
 ) {
-  const formatted_day = String(date.getDate()).padStart(rightJustifiedValue);
-  if (date.getDay() === saturday) {
+  const formatted_day = String(date.getDate()).padStart(RIGHT_JUSTIFIED_LENGTH);
+  if (date.getDay() === SATURDAY) {
     console.log(formatted_day);
   } else {
     process.stdout.write(`${formatted_day} `);
