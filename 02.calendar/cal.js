693648eec33189c7a47ff8fc7bb8b2f4738824d9
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-03-11 18:00:15 +0900

    正常に動作する、最初の状態を作成

---
+++ 02.calendar/cal.js
@@ -0,0 +1,22 @@
+const args = require("minimist")(process.argv.slice(2));
+const today = new Date();
+const month = args.m || today.getMonth() + 1;
+const year = args.y || today.getFullYear();
+const correctionValue = 1;
+const startDate = new Date(year, month - correctionValue);
+const endDate = new Date(year, month, 0);
+const defaultSpaceValue = "   ";
+const rightJustifiedValue = 2;
+
+console.log(`     ${month}月 ${year}`);
+console.log("日 月 火 水 木 金 土");
+const defaultSpace = defaultSpaceValue.repeat(startDate.getDay());
+process.stdout.write(defaultSpace);
+for (startDate; startDate <= endDate; startDate.setDate(startDate.getDate() + 1)) {
+  let day = String(startDate.getDate()).padStart(rightJustifiedValue, defaultSpaceValue);
+  if (startDate.getDay() == 6) {
+    console.log(day);
+  } else{
+    process.stdout.write(`${day} `);
+  }
+}
