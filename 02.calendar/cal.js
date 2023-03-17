fc173a3d1dd38e6b43b4fd73b2e32bc622027039
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-03-17 10:12:50 +0900

    不要な定数の削除と、変数名の見直しを行う

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,27 +1,25 @@
 const args = require("minimist")(process.argv.slice(2));
 
 const today = new Date();
-const VALUE_FOR_GET_MONTH_CORRECTLY = 1;
-const month = args.m || today.getMonth() + VALUE_FOR_GET_MONTH_CORRECTLY;
+const OFFSET_FOR_GET_MONTH_CORRECTLY = 1;
+const month = args.m || today.getMonth() + OFFSET_FOR_GET_MONTH_CORRECTLY;
 const year = args.y || today.getFullYear();
-const startDate = new Date(year, month - VALUE_FOR_GET_MONTH_CORRECTLY);
-const VALUE_FOR_GET_MONTH_END = 0;
-const endDate = new Date(year, month, VALUE_FOR_GET_MONTH_END);
+const startDate = new Date(year, month - OFFSET_FOR_GET_MONTH_CORRECTLY);
+const endDate = new Date(year, month, 0);
 
-const DEFAULT_SPACE = "   ";
-const RIGHT_JUSTIFIED_LENGTH = 2;
+const DISPLAYED_DATE_LENGTH = 2;
 const SATURDAY = 6;
 
 console.log(`     ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
-const firstWeeksSpace = DEFAULT_SPACE.repeat(startDate.getDay());
-process.stdout.write(firstWeeksSpace);
+const BLANK_DATE = "   ".repeat(startDate.getDay());
+process.stdout.write(BLANK_DATE);
 for (
   const date = startDate;
   date <= endDate;
   date.setDate(date.getDate() + 1)
 ) {
-  const formattedDay = String(date.getDate()).padStart(RIGHT_JUSTIFIED_LENGTH);
+  const formattedDay = String(date.getDate()).padStart(DISPLAYED_DATE_LENGTH);
   if (date.getDay() === SATURDAY) {
     console.log(formattedDay);
   } else {
